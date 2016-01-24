a0199ae14a4f51c7f93e3296689fa1ae47d067b8
810 <info@jellekoksolutions.nl>
2016-01-24 18:16:42 +0100

    When Direct Component Access and Protect Attachments is active, Images #3647

--- components/com_kunena/admin/template/joomla30/layouts/attachment/item/default.php
+++ components/com_kunena/admin/template/joomla30/layouts/attachment/item/default.php
@@ -12,11 +12,37 @@ defined ( '_JEXEC' ) or die ();
 
 /** @var KunenaAttachment $attachment */
 $attachment = $this->attachment;
+
+if ($attachment->protected && !KunenaFactory::getConfig()->access_component)
+{
+	$url_href = 'forum/attachment/' . $attachment->id;
+	$url_href2 = JUri::root() . $url_href ;
+}
+elseif ($attachment->protected)
+{
+	$url_href = $attachment->getUrl();
+}
+else
+{
+	$url_href = JUri::root() . $attachment->getUrl();
+}
 ?>
-<a href="<?php echo JUri::root() . $attachment->getUrl(); ?>" title="<?php echo $attachment->getFilename(); ?>">
-	<?php if ($attachment->isImage())
+
+<?php if ($attachment->protected && !KunenaFactory::getConfig()->access_component) : ?>
+	<a href="<?php echo $url_href2; ?>" title="<?php echo $attachment->getFilename(); ?>">
+<?php else : ?>
+	<a href="<?php echo $url_href; ?>" title="<?php echo $attachment->getFilename(); ?>">
+<?php endif; ?>
+
+<?php
+
+	if ($attachment->isImage() && !$attachment->protected)
 	{
 		echo '<img src="' . JUri::root() . $attachment->getUrl(true) . ' " height="40" width="40" />';
+	}
+	elseif ($attachment->isImage() && $attachment->protected)
+	{
+		echo '<img src="' . JUri::root() . $url_href . ' " height="40" width="40" />';
 	}
 	else
 	{
@@ -24,3 +50,4 @@ $attachment = $this->attachment;
 	}
 	?>
 </a>
+
